8640ccc0eb3ebf8decae912f7f7e2e9ea7502b79
destifo <estifanosbireda@gmail.com>
2022-03-17 16:17:21 +0300

    added codes on node modules practices

---
+++ Node js/destifo/README.md
@@ -0,0 +1,4 @@
+# About
+
+Estifanos Bireda
+<https://www.linkedin.com/in/estifanos-b-49360a104/>
---
+++ Node js/destifo/index.js
@@ -0,0 +1,9 @@
+
+module.exports.destifo = () => console.log(`The World, 
+Here I Come`);
+
+module.exports.about = () => ({
+    name: "Estifanos Bireda",
+    occupation: "3rd year Software Engineering student",
+    linkedin: "https://www.linkedin.com/in/estifanos-b-49360a104/"
+});
