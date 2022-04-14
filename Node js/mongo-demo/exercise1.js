2fa0fe249c74c9682fda21cd8e2205734959fc6f
destifo <estifanosbireda@gmail.com>
2022-04-14 20:10:20 +0300

    added the solutions to the two exercises

---
+++ Node js/mongo-demo/exercise1.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/mongo-exercises').then(() => console.log("Connected to MongoDB...")).catch(err => console.log("Error connecting", err));
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    tags: [ String ],
+    author: String,
+    date: {type: Date, default:Date.now},
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model("courses", courseSchema);
+
+async function getCoursesByFilter() {
+    const courses = await Course
+        .find()
+        .and([{tags: { $in: ['backend']}}, {isPublished: true}])
+        .sort({name: 1})
+        .select({name: 1, author: 1});
+
+    return courses;
+}
+
+async function run() {
+    const courses = await getCoursesByFilter2();
+    console.log(courses);
+}
+
+async function getCoursesByFilter2() {
+    const courses = await Course
+        .find({tags: { $in: ['backend', 'frontend']}})
+        .sort({price: -1})
+        .select({name: 1, author: 1});
+
+    return courses;
+}
+
+
+run();
+
