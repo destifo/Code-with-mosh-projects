483a9b06b7728421c8e0b09832aa2fe6a13ba186
destifo <estifanosbireda@gmail.com>
2022-08-01 21:00:02 +0300

    added the remove, traversal and autocomplete methods

--- DS_Algorithms/DS/ds-algorithms/src/main/java/com/destifo/Tries/Trie.java
+++ DS_Algorithms/DS/ds-algorithms/src/main/java/com/destifo/Tries/Trie.java
@@ -1,13 +1,13 @@
 package com.destifo.Tries;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Trie {
     
     private class TrieNode {
-        public char ch;
-        private static int ALPHABET_SIZE = 26;
+        private char ch;
         public Map<Character, TrieNode> children = new HashMap<Character, TrieNode>();
         public boolean isEndOfword = false; 
 
@@ -15,8 +15,16 @@ public class Trie {
             this.ch = c;
         }
 
+        public char getValue() {
+            return ch;
+        }
+
         public boolean hasChild(char ch) {
             return children.containsKey(ch);
+        }
+
+        public boolean hasChildren() {
+            return !children.isEmpty();
         }
 
         public void addChild(char ch) {
@@ -25,6 +33,14 @@ public class Trie {
 
         public TrieNode getChild(char ch) {
             return children.get(ch);
+        }
+        
+        public TrieNode[] getChildren() {
+            return children.values().toArray(new TrieNode[0]);
+        }
+
+        public void removeChild(TrieNode node) {
+            children.remove(node);
         }
         
         @Override
@@ -73,6 +89,102 @@ public class Trie {
             curr = curr.getChild(letter);
         }
         curr.isEndOfword = true;
+    }
+
+    public void traversePreOrder() {
+
+        for (TrieNode node: root.getChildren())
+            traversePreOrder(node);
+        
+    }
+
+    private void traversePreOrder(TrieNode root) {
+        System.out.println(root.getValue());
+
+        for (TrieNode node: root.getChildren())
+            traversePreOrder(node);
+        
+    }
+
+    public void traversePostOrder() {
+        for (TrieNode node: root.getChildren())
+            traversePreOrder(node);
+        
+    }
+
+    private void traversePostOrder(TrieNode root) {
+        for (TrieNode node: root.getChildren()) 
+            traversePreOrder(node);
+        
+        System.out.println(root.getValue());
+    }
+
+    public void remove(String word) {
+        if (word == null || word.length() < 1){
+            return;
+        }
+
+        char firstLetter = word.charAt(0);
+        if (!root.hasChild(firstLetter))
+            return;
+
+        var curr = root.getChild(firstLetter);
+        boolean removeCurr = remove(curr, word, 1);
+        if (removeCurr)
+            root.removeChild(curr);
+    }
+
+    private boolean remove(TrieNode root, String word, int depth) {
+        if (word.length() == depth) {
+            root.isEndOfword = false;
+            if (root.getChildren().length == 0)
+                return true;
+            return false;
+        }
+        else{
+            var child = root.getChild(word.charAt(depth));
+            if (child == null)
+                return false;
+            boolean removeChild = remove(child, word, ++depth);
+
+            if (removeChild){
+                root.removeChild(child);
+                if (!root.isEndOfword)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public ArrayList<String> autoComplete(String prefix) {
+        ArrayList<String> suggestions = new ArrayList<>();
+        if (prefix == null)
+            return suggestions;
+
+        var curr = root;
+        for (char ch : prefix.toCharArray()){
+            curr = curr.getChild(ch);
+            if (curr == null)
+                return suggestions;
+        }
+        
+        String word = prefix;
+        autoComplete(curr, word, suggestions);
+
+        return suggestions;
+    }
+
+    private void autoComplete(TrieNode root, String word, ArrayList<String> suggestions) {
+        if (root.isEndOfword)
+            suggestions.add(word);
+        
+        if (!root.hasChildren())
+            return;
+
+        for (TrieNode node : root.getChildren()){
+            String nextWord = (word + node.ch);
+            autoComplete(node, nextWord, suggestions);
+        }
     }
 
 }
--- DS_Algorithms/DS/ds-algorithms/src/main/java/com/destifo/Tries/TrieTest.java
+++ DS_Algorithms/DS/ds-algorithms/src/main/java/com/destifo/Tries/TrieTest.java
@@ -6,6 +6,12 @@ public class TrieTest {
         Trie trie = new Trie();
         trie.insert("dop");
         trie.insert("dog");
+        trie.insert("dope");
+        System.out.println(trie.autoComplete(""));
+        // trie.traversePreOrder();
+        // trie.remove("dog");
+        // System.out.println(trie.contains("dog"));
+        // System.out.println(trie.contains("dop"));
         System.out.println("debug line");
     }
 
